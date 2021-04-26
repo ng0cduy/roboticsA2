14de268b44815e37c316257a63956d8278344743
ng0cduy <49388191+ng0cduy@users.noreply.github.com>
2021-04-27 03:11:43 +0700

    fix DH-param + modify IKine + Reset function

--- denso model/DensoVS060.m
+++ denso model/DensoVS060.m
@@ -1,4 +1,7 @@
 classdef DensoVS060<handle
+    properties (Constant)
+        qz = zeros(1,7);
+    end
     properties(Access =public)
         base;
         endEffector;
@@ -16,19 +19,18 @@ classdef DensoVS060<handle
             self.useGripper = useGripper;  
             self.base = base;
             self.name = name;
-            
             self.getDensoVS060();
             self.SetBase(base);
         end
         %% Create Denso Links
         function getDensoVS060(self)       
             L1 = Link([pi     0       0       pi/2    1.001]);
-            L2=Link('alpha',pi/2,'a',0, 'd',0.145, 'offset',pi,'qlim',[deg2rad(-360), deg2rad(360)] );
-            L3=Link('alpha',0,'a',0.308, 'd',0, 'offset',pi/2,'qlim',[deg2rad(-120), deg2rad(120)]);
-            L4=Link('alpha',-pi/2,'a',0, 'd',0, 'offset',0,'qlim',[deg2rad(-160), deg2rad(160)]);
-            L5=Link('alpha',pi/2,'a',0, 'd',0.31, 'offset',0,'qlim',[deg2rad(-180), deg2rad(180)]);
-            L6=Link('alpha',-pi/2,'a',0, 'd',0, 'offset',pi/2,'qlim',[deg2rad(-360), deg2rad(360)]);
-            L7=Link('alpha',0,'a',0, 'd',0.06, 'offset',0,'qlim',[deg2rad(360), deg2rad(360)]);
+            L2=Link('alpha',pi/2,'a',0, 'd',0.148, 'offset',pi,'qlim',[deg2rad(-170), deg2rad(170)] );
+            L3=Link('alpha',0,'a',0.305, 'd',0, 'offset',pi/2,'qlim',[deg2rad(-120), deg2rad(120)]);
+            L4=Link('alpha',-pi/2,'a',0, 'd',0, 'offset',0,'qlim',[deg2rad(-125), deg2rad(155)]);
+            L5=Link('alpha',pi/2,'a',0, 'd',0.3, 'offset',0,'qlim',[deg2rad(-270), deg2rad(270)]);
+            L6=Link('alpha',-pi/2,'a',0, 'd',0, 'offset',pi/2,'qlim',[deg2rad(-120), deg2rad(120)]);
+            L7=Link('alpha',0,'a',0, 'd',0.06, 'offset',0,'qlim',[deg2rad(-360), deg2rad(360)]);
             L1.qlim = [-0.8 0];            
             self.model = SerialLink([L1 L2 L3 L4 L5 L6 L7],'name',self.name);
 %             self.model.teach();
@@ -66,20 +68,53 @@ classdef DensoVS060<handle
             end
         end
         %% ForWard Kinematic for UR3
-        function FKine(self,q)
-              self.endEff = self.model.fkine(q);
+        function ee_pose = FKine(self,q)
+              self.endEffector = self.model.fkine(q);
+              ee_pose = self.endEffector;
         end
         %% InverseKinematic for UR3
-        function IKine(self,transform)
+        function q_ = IKine(self,transform)
     %           self.pose=transform;
-              self.pose=self.model.ikcon(transform);
+              steps = 15;
+              poseArr   = cell(1,steps);
+              qArr      = cell(1,steps);
+              rotMat    = cell(1,steps);
+              transMat  = cell(1,steps);
+              errRotMat    = cell(1,steps);
+              errRot    = cell(1,steps);
+              errTransl  = cell(1,steps);
+              errTot    = cell(1,steps);
+              stepsQ = (self.model.qlim(:,2) - self.model.qlim(:,1))/steps; 
+              [Rd,Td] = tr2rt(transform);
+              for i = 0:1:steps                
+                  testQ                         = self.model.qlim(:,1) + i* stepsQ;
+                  qArr{i+1}                     = self.model.ikcon(transform,testQ');
+                  poseArr{i+1}                  = self.FKine(qArr{i+1});
+                  [rotMat{i+1},transMat{i+1}]   = tr2rt(poseArr{i+1});
+                  errRotMat{i+1}                = Rd * (rotMat{i+1})';
+                  errRot{i+1}                   = norm(errRotMat{i+1} - eye(3));
+                  errTransl{i+1}                = norm (transMat{i+1} - Td);
+                  errTot{i+1}                   = errRot{i+1} * errTransl{i+1};                   
+              end
+              [minErr,errIndex] = min([errTot{:}]);
+              q_ = qArr{errIndex}; 
         end
+        %% Reset the robot
+        function Reset(self)
+               poseNew = self.FKine(self.qz);
+               if self.model.getpos() ~= self.qz
+                   self.Animate(poseNew,50);
+               else
+                   self.model.animate(self.qz);
+               end
+        end
+        
         %% Trajectory using Quintic Polynomial 
-        function Animate(self,pose)
-              self.IKine(pose);
-              self.qMatrix = jtraj(self.model.getpos, self.pose, 15);
+        function Animate(self,pose,steps)
+              qNew = self.IKine(pose);
+              self.qMatrix = jtraj(self.model.getpos, qNew,steps);
 %               self.initialPose = self.model.getpos;
-              for i = 1:1:15
+              for i = 1:1:steps
                    self.model.animate(self.qMatrix(i,:));
                    drawnow();
               end
--- denso model/testmodel.m
+++ denso model/testmodel.m
@@ -1,7 +1,21 @@
-close all;
-
+% close all;
+clf
+clear all
+%%
 a = DensoVS060(false,transl(0,0,0),'denso');
 a.model.teach;
+
 % keyboard;
 % temp= [0.6,0.5,0.1];
 % a.Animate(transl(temp));
+%%
+clc
+pose = transl(0,0,0.5)*trotz(45,'deg');
+q = a.IKine(pose);
+a.Animate(pose,50);
+% a.model.animate(q);
+a.Reset()
+% a.Animate(pose,15);
+% q = a.model.getpos();
+% b = cell(1,15)
+% b{1} = transl(1,1,1)
