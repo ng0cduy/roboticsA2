2c293377eb0e91e5cf806f08d993b9c241aaffc9
Trong Duy Bui <13432190@student.uts.edu.au>
2021-05-01 23:15:23 +0700

    update light curtain object

--- denso model/LightCurtain.m
+++ denso model/LightCurtain.m
@@ -4,28 +4,27 @@ classdef LightCurtain<handle
     end
     properties(Access =public)
          verts;
-         a=1;
+         a=2;
          z=0;
+         dist=2;
     end
     methods(Access = public)
         function self = LightCurtain()
-            for i = -self.a:0.1:self.a
+            for i = -self.a:0.04:self.a
                 for y = 1:4
                     hold on;
                     if y==1
-                        tr = transl(i,1,self.z);
+                        tr = transl(i,self.a,self.z);
                     elseif y == 2
-                        tr = transl(1,i,self.z);
+                        tr = transl(self.a,i,self.z);
                     elseif y==3
-                        tr = transl(i,-1,self.z);
+                        tr = transl(i,-self.a,self.z);
                     else
-                        tr = transl(-1,i,self.z);
+                        tr = transl(-self.a,i,self.z);
                     end
                     startP = tr(1:3,4)';
 
-                    dist = 1;
-
-                    endP = tr(1:3,4)' + dist * tr(1:3,3)';
+                    endP = tr(1:3,4)' + self.dist * tr(1:3,3)';
                     line1_h = plot3([startP(1),endP(1)],[startP(2),endP(2)],[startP(3),endP(3)],'r');
                     plot3(endP(1),endP(2),endP(3),'r');
                     axis equal;
