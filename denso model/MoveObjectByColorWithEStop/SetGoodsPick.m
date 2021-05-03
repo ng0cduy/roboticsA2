c607500f144bc093a668211f814d5630c606d8c0
Trong Duy Bui <13432190@student.uts.edu.au>
2021-05-03 21:50:04 +0700

    update environment

--- denso model/MoveObjectByColorWithEStop/SetGoodsPick.m
+++ denso model/MoveObjectByColorWithEStop/SetGoodsPick.m
@@ -1,8 +1,8 @@
 function boxes = SetGoodsPick
     goodsX = 0.3;
-    goodsY = 0.3;
+    goodsY = 0;
     goodsBaseZ = 0;
-    goodsBaseZ = goodsBaseZ+0.1; % for the goods's pose position at the center of the goods
+    goodsBaseZ = goodsBaseZ+0.06; % for the goods's pose position at the center of the goods
                                  % z of goods = 0.2;
     
     boxes = cell(3,1);
@@ -11,11 +11,11 @@ function boxes = SetGoodsPick
         hold on
         switch choice
             case 1
-                boxes{i} = goods('red.ply',transl(goodsX,goodsY,goodsBaseZ+0.2*(i-1))*troty(pi)); %z of goods = 0.2; 
+                boxes{i} = goods('red.ply',transl(goodsX,goodsY,goodsBaseZ+0.12*(i-1))*troty(pi)); %z of goods = 0.2; 
             case 2 
-                boxes{i} = goods('blue.ply',transl(goodsX,goodsY,goodsBaseZ+0.2*(i-1))*troty(pi));
+                boxes{i} = goods('blue.ply',transl(goodsX,goodsY,goodsBaseZ+0.12*(i-1))*troty(pi));
             otherwise
-                boxes{i} = goods('green.ply',transl(goodsX,goodsY,goodsBaseZ+0.2*(i-1))*troty(pi));
+                boxes{i} = goods('green.ply',transl(goodsX,goodsY,goodsBaseZ+0.12*(i-1))*troty(pi));
         end
     end
 end
